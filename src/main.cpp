5e416e0a789f5ec39f80b95dc2396a26c8ba78f7
Brian Colombini <brianjcolombini@ip0af5213b.int.uvm.edu>
2017-05-04 16:23:18 -0400

    don't need this score initialization

--- src/main.cpp
+++ src/main.cpp
@@ -67,8 +67,6 @@ namespace game {
         gameState.player->setGrapplingHook(grapplingHook);
         insertEntity(grapplingHook, gameState.entities);
         insertEntity(gameState.player, gameState.entities);
-        gameState.score = make_shared<Score>();
-        insertEntity(gameState.score, gameState.entities);
     }
 
     void update() {
